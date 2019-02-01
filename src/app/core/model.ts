c803205dca54a3f0a4e671c9d6636b0f03309f25
Julio Cesar Marques de Mendonça <julio.mendonca@matera.com>
2019-02-01 14:37:59 -0200

    14#

--- src/app/core/model.ts
+++ src/app/core/model.ts
@@ -1,11 +1,38 @@
-export class Lancamento {
-  codigo: number;
-  tipo = 'RECEITA';
-  descricao: string;
-  dataVencimento: Date;
+export class Cesta {
+  id: number;
+  indFinalizado: string;
+  dataHoraCadastro: Date;
+  beneficios: [];
   dataPagamento: Date;
   valor: number;
   observacao: string;
   anexo: string;
   urlAnexo: string;
+}
+
+export class Beneficiario {
+  type: string;
+  codigo: string;
+  nome: string;
+  cpf: number;
+  rg: number;
+  empresa: string;
+  cnpj: number;
+  cidade: string;
+  dataNascimento: number;
+  ativo: string;
+  email: string;
+}
+
+export class Sede {
+  codigo: string;
+  descricao: string;
+  codigoProjetoAssento: string;
+}
+
+export class Cargo {
+  codigoNivelCargo: string;
+  descricao: string;
+  ativo: string;
+  valorBeneficio: number;
 }
